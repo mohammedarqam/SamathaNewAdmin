17c71e57841160a27d126979ef8346671b10cc20
Mohammed Arqam <mohammedarqam97@gmail.com>
2018-12-01 00:02:42 +0530

    dashboard Charts started

--- src/pages/Extra/dashboard/dashboard.ts
+++ src/pages/Extra/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController, LoadingController, ModalController } from 'ionic-angular';
 import { AngularFireDatabase } from '@angular/fire/database';
 import * as firebase from 'firebase';
@@ -12,13 +12,21 @@ import { VillageDetailsPage } from '../../Subs/Villages/village-details/village-
 import { AnmDetailsPage } from '../../Subs/Anms/anm-details/anm-details';
 import { DeleteStudentsPage } from '../../Students/delete-students/delete-students';
 import { EditStudentPage } from '../../Students/edit-student/edit-student';
-
+import { Chart } from 'chart.js';
 @IonicPage()
 @Component({
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
 })
 export class DashboardPage {
+  @ViewChild('barCanvas1') barCanvas1;
+  @ViewChild('barCanvas2') barCanvas2;
+ 
+  barChart1: any;
+  barChart2: any;
+
+  showChar : boolean = true;
+
   pageName = "Dashboard";
   districtSel = "Wanaparthy";
 
@@ -72,7 +80,90 @@ export class DashboardPage {
   }
   ngOnInit() {
     this.getStudents();
+    
   }
+
+  loadCharts(){
+    this.barChart1 = new Chart(this.barCanvas1.nativeElement, {
+
+      type: 'bar',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+
+  });
+
+    this.barChart2 = new Chart(this.barCanvas2.nativeElement, {
+
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+
+    });
+
+  }
+
   getStudents(){
     let loading = this.loadingCtrl.create({
       content: 'Please wait...'
@@ -448,5 +539,10 @@ export class DashboardPage {
       schho.key = itemSnap.key;
     });
     this.navCtrl.push(AnmDetailsPage,{ anm: schho});
+  }
+
+  togCharts(){
+    this.showChar = !this.showChar;
+    this.loadCharts();
   }
 }
